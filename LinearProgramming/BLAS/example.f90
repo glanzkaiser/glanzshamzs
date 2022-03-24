e72c182e37cf2a8fd9399b5bb3c12a23ff79b9f6
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-03-25 04:16:22 +0700

    Create example.f90

---
+++ LinearProgramming/BLAS/example.f90
@@ -0,0 +1,17 @@
+program main
+    implicit none (type, external)
+    external :: sscal
+
+    integer, parameter :: N = 3
+
+    real :: x(N) = [ 5., 6., 7. ]
+    real :: a    = 5.
+
+    print '("a = ", f0.1)' a
+    print '("X = [ ", 3(f0.1, " "), "]")', x
+
+    call sscal(N, a, x, 1)
+
+    print '(/, "X = a * X")'
+    print '("X = [ ", 3(f0.1, " "), "]")', x
+end program main
