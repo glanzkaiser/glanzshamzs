1b65ef4c8055f2ee95f2aa07bac288193aac990d
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-04-03 21:26:47 +0700

    Update ARIMA.py

--- Python/MachineLearning/timeseries/ARIMA.py
+++ Python/MachineLearning/timeseries/ARIMA.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import datetime
+import matplotlib.dates as mdates
 sns.set()
 
 dis = pd.read_csv('DIS.csv')
@@ -32,13 +34,22 @@ def auto_regressive(signal, p, d, q, future_count = 10):
 
 future_count = 15
 
+formatter = mdates.DateFormatter("%Y") ### formatter of the date
+locator = mdates.YearLocator() ### where to put the labels
+
 predicted_15 = auto_regressive(signal,15,1,2,future_count)
 predicted_30 = auto_regressive(signal,30,1,2,future_count)
 
 plt.figure(figsize=(15, 7))
+ax = plt.gca()
+ax.xaxis.set_major_formatter(formatter) ## calling the formatter for the x-axis
+ax.xaxis.set_major_locator(locator) ## calling the locator for the x-axis
+
 plt.plot(signal, label = 'DIS')
 plt.plot(predicted_15, label = 'ARIMA 15 MA')
 plt.plot(predicted_30, label = 'ARIMA 30 MA')
+plt.xlabel('Earth Year')
+plt.ylabel('Price') 
 plt.legend()
 plt.show()
 
