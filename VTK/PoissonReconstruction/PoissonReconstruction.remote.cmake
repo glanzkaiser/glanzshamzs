316840fb455fc0abe8d2516ad7109a8ed48940ae
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-04-11 04:27:55 +0700

    Create PoissonReconstruction.remote.cmake

---
+++ VTK/PoissonReconstruction/PoissonReconstruction.remote.cmake
@@ -0,0 +1,10 @@
+#
+# Poisson Reconstruction
+#
+
+vtk_fetch_module(PoissonReconstruction
+  "Poisson Surface reconstruction from unorganized points"
+  GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
+  # September 10, 2017 refactored remote module
+  GIT_TAG f3af082c2093fa3e40922daf4bbe16e5f53b0201
+  )
