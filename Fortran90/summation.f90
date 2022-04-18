793ff9572be66b846ee22b9eb224b5d8018cdad5
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-04-18 17:58:03 +0700

    Update summation.f90

--- Fortran90/summation.f90
+++ Fortran90/summation.f90
@@ -2,7 +2,7 @@ program summation
     implicit none
     integer :: sum, a
 
-    print *, "This program performs summations till 10 numbers. Enter 0 to stop."
+    print *, "This program performs summations. Enter 0 to stop."
     open (unit=10, file="SumData.DAT")
     sum = 0
     do
