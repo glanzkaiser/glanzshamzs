3c4b703629b07e6a269a33cd1ce6aba50156e5f2
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-04-18 17:53:41 +0700

    Create summation.f90

---
+++ Fortran90/summation.f90
@@ -0,0 +1,22 @@
+program summation
+    implicit none
+    integer :: sum, a
+
+    print *, "This program performs summations till 10 numbers. Enter 0 to stop."
+    open (unit=10, file="SumData.DAT")
+    sum = 0
+    do
+        print *, "Add:"
+        read *, a
+        if (a == 0) then
+            exit
+        else
+            sum = sum + a
+        end if
+        write (10,*) a
+    end do
+
+    print *, "Summation =", sum
+    write (10,*) "Summation =", sum
+    close(10)
+end
