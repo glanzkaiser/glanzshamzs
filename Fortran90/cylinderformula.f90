4cfbd3ef811587ee08e92137c5fcf0c066a2bb68
glanzkaiser <72222484+glanzkaiser@users.noreply.github.com>
2022-04-19 14:49:22 +0700

    Update and rename cylinderarea.f90 to cylinderformula.f90

--- Fortran90/cylinderformula.f90
+++ Fortran90/cylinderformula.f90
@@ -10,7 +10,7 @@ program cylinder
 
   integer :: ierr
   character(1) :: yn
-  real :: radius, height, area
+  real :: radius, height, area, volume
   real, parameter :: pi = 3.141592653589793
 
   interactive_loop: do
@@ -34,13 +34,13 @@ program cylinder
 !   Compute area.  The ** means "raise to a power."
 
     area = 2*pi * (radius**2 + radius*height)
-
+    volume = pi * ((radius**2) * (height))
+  
 !   Write the input variables (radius, height)
-!   and output (area) to the screen.
+!   and output (area, volume) to the screen.
 
     write (*,'(1x,a7,f6.2,5x,a7,f6.2,5x,a5,f6.2)') &
-      'radius=',radius,'height=',height,'area=',area
-
+      'radius=',radius,'height=',height,'area=',area, 'volume=',volume
     yn = ' '
     yn_loop: do
       write(*,*) 'Perform another calculation? y[n]'
