18e33e13442f02d074f1bde0db08b50a736ff23e
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-04-29 22:11:38 +0700

    Create iris1.jl

---
+++ Julia/Plots/iris1.jl
@@ -0,0 +1,26 @@
+using Plots; theme(:ggplot2)
+using RDatasets
+
+iris = dataset("datasets", "iris");
+
+X = iris.PetalLength
+Y = iris.SepalLength
+sizes = iris.SepalWidth
+labels = iris.Species
+
+# Scatter plot with some custom settings
+scatter(X,
+    Y,
+    xlim = (0, 7.5),
+    ylim = (3.5, 8.5),
+    group = labels,
+    legend = :topleft,
+    title = "The Iris Dataset",
+    xlabel = "Petal Length",
+    ylabel = "Sepal Length",
+    color = [:darkviolet : green3 :red3],
+    markersize = sizes,
+    markershape = [:hex :star7 :cross],
+    alpha = 0.60,
+    dpi = 1000
+)
