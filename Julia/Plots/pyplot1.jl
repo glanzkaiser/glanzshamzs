d7fafc7ba175b93e57ffca167fee48731d2d5cdd
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-04-30 22:04:34 +0700

    Update pyplot1.jl

--- Julia/Plots/pyplot1.jl
+++ Julia/Plots/pyplot1.jl
@@ -4,9 +4,6 @@ using PyPlot
 globaltemperatures = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8];
 numindustries = [17, 400, 5000, 15000, 20000, 45000];
  
-#use PyPlot
-Plots.pyplot()
- 
 #plot
 plot(numindustries, globaltemperatures, label="line")
 #add points
@@ -16,5 +13,3 @@ scatter!(numindustries, globaltemperatures, label="points")
 xlabel!("Number of Industries")
 ylabel!("Global Temperature (°C)")
 
-#save plot
-png("/opt/hamzstlib/glanzshamzs/glanzshamzs/Julia")
