3b4908b3af21a1f199ed7cc7b5e99196e336ff38
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-04-30 22:02:59 +0700

    Update iris2.jl

--- Julia/Plots/iris2.jl
+++ Julia/Plots/iris2.jl
@@ -1,20 +1,16 @@
-using RDatasets
-using PyPlot
-iris = dataset("datasets", "iris");
-
-# load the StatsPlots recipes (for DataFrames) available via:
-# Pkg.add("StatsPlots")
-using StatsPlots
-
-# Scatter plot with some custom settings
-scatter(
-    :SepalLength,
-    :SepalWidth,
-    group = :Species,
-    title = "My awesome plot",
-    xlabel = "Length",
-    ylabel = "Width",
-    m = (0.5, [:cross :hex :star7], 12),
-    bg = RGB(0.2, 0.2, 0.2)
-)
-PyPlot.title("Scatter Plot")
+# Using the DataFrames Package
+using DataFrames
+   
+# Using the CSV package
+using CSV
+   
+# Reading the CSV file
+Iris = CSV.read("Iris.csv");
+  
+# Using Plots Package 
+using Plots 
+  
+# Plot Histogram
+Plots.histogram(Iris[:SepalLength], 
+                bins = 50, xlabel = "SepalLength", 
+                            labels = "Length in cm")
