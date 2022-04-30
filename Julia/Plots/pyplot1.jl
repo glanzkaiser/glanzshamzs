65f7b72c66b5c9dea5b39bd7676f46e2ba2a610c
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-04-30 07:47:48 +0700

    Create pyplot1.jl

---
+++ Julia/Plots/pyplot1.jl
@@ -0,0 +1,17 @@
+using Plots
+ 
+#data to plot
+globaltemperatures = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8];
+numindustries = [17, 400, 5000, 15000, 20000, 45000];
+ 
+#use PyPlot
+Plots.pyplot()
+ 
+#plot
+plot(numindustries, globaltemperatures, label="line")
+#add points
+scatter!(numindustries, globaltemperatures, label="points")
+ 
+#adding labels to plot
+xlabel!("Number of Industries")
+ylabel!("Global Temperature (°C)")
