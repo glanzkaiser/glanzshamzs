9f961b96325470b30db09c16b5043899e24112da
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-04-30 23:02:09 +0700

    Create 3dplot.jl

---
+++ Julia/Plots/3dplot.jl
@@ -0,0 +1,10 @@
+using Plots
+pyplot()
+
+n = 100
+ts = range(0, stop = 8π, length = n)
+x = ts .* map(cos, ts)
+y = (0.1ts) .* map(sin, ts)
+z = 1:n
+plot(x, y, z, zcolor = reverse(z), m = (10, 0.8, :blues, Plots.stroke(0)), leg = false, cbar = true, w = 5)
+plot!(zeros(n), zeros(n), 1:n, w = 10)
