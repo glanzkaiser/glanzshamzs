3c73f5ceab86d4067dba7df76bb91de6ceac0f27
glanzkaiser <root@GFreya.freya.valhalla>
2022-05-04 13:12:42 +0000

    Add GLFW examples

---
+++ Box2d/HelloWorld/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/HelloWorld.o"
+  "CMakeFiles/HelloWorld.dir/HelloWorld.o.d"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
