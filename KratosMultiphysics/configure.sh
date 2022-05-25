2fdb850de1271c9ef8503cd8c97bf25116f56f3c
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-05-25 20:53:07 +0700

    Update configure.sh

--- KratosMultiphysics/configure.sh
+++ KratosMultiphysics/configure.sh
@@ -31,9 +31,57 @@ export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
 
 # Set applications to compile
 export KRATOS_APPLICATIONS=
-add_app ${KRATOS_APP_DIR}/LinearSolversApplication
-add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
+add_app ${KRATOS_APP_DIR}/CableNetApplication
+add_app ${KRATOS_APP_DIR}/ChimeraApplication 
+add_app ${KRATOS_APP_DIR}/CoSimulationApplication
+add_app ${KRATOS_APP_DIR}/CompressiblePotentialFlowApplication 
+add_app ${KRATOS_APP_DIR}/ConstitutiveLawsApplication
+add_app ${KRATOS_APP_DIR}/ConstitutiveModelsApplication
+add_app ${KRATOS_APP_DIR}/ContactMechanicsApplication
+add_app ${KRATOS_APP_DIR}/ContactStructuralMechanicsApplication 
+add_app ${KRATOS_APP_DIR}/ConvectionDiffusionApplication
+add_app ${KRATOS_APP_DIR}/DEMApplication
+add_app ${KRATOS_APP_DIR}/DamApplication
+add_app ${KRATOS_APP_DIR}/DelaunayMeshingApplication
+add_app ${KRATOS_APP_DIR}/DemStructuresCouplingApplication 
+add_app ${KRATOS_APP_DIR}/EigenSolversApplication
+add_app ${KRATOS_APP_DIR}/ExaquteSandboxApplication
+add_app ${KRATOS_APP_DIR}/FSIApplication
+add_app ${KRATOS_APP_DIR}/FemToDemApplication 
 add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+add_app ${KRATOS_APP_DIR}/FluidDynamicsBiomedicalApplication
+add_app ${KRATOS_APP_DIR}/FluidTransportApplication 
+add_app ${KRATOS_APP_DIR}/FreeSurfaceApplication
+add_app ${KRATOS_APP_DIR}/GeoMechanicsApplication 
+add_app ${KRATOS_APP_DIR}/HDF5Application 
+add_app ${KRATOS_APP_DIR}/IgaApplication
+add_app ${KRATOS_APP_DIR}/LinearSolversApplication
+add_app ${KRATOS_APP_DIR}/MappingApplication
+add_app ${KRATOS_APP_DIR}/MeshMovingApplication
+add_app ${KRATOS_APP_DIR}/MeshingApplication
+add_app ${KRATOS_APP_DIR}/MetisApplication
+add_app ${KRATOS_APP_DIR}/MultilevelMonteCarloApplication
+add_app ${KRATOS_APP_DIR}/PFEM2Application
+add_app ${KRATOS_APP_DIR}/ParticleMechanicsApplication
+add_app ${KRATOS_APP_DIR}/PfemApplication
+add_app ${KRATOS_APP_DIR}/PfemFluidDynamicsApplication
+add_app ${KRATOS_APP_DIR}/PfemSolidMechanicsApplication
+add_app ${KRATOS_APP_DIR}/PoromechanicsApplication
+add_app ${KRATOS_APP_DIR}/RANSApplication
+add_app ${KRATOS_APP_DIR}/RomApplication
+add_app ${KRATOS_APP_DIR}/ShallowWaterApplication
+add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication
+add_app ${KRATOS_APP_DIR}/SolidMechanicsApplication
+add_app ${KRATOS_APP_DIR}/StatisticsApplication
+add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
+add_app ${KRATOS_APP_DIR}/SwimmingDEMApplication
+add_app ${KRATOS_APP_DIR}/ThermalDEMApplication
+add_app ${KRATOS_APP_DIR}/TopologyOptimizationApplication
+add_app ${KRATOS_APP_DIR}/TrilinosApplication
+add_app ${KRATOS_APP_DIR}/ULFapplication
+add_app ${KRATOS_APP_DIR}/UmatApplication
+add_app ${KRATOS_APP_DIR}/WindEngineeringApplication 
+
 
 # Clean
 clear
