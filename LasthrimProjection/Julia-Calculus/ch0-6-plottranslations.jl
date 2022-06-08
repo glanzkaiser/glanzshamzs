cab5924328070cb05a207e7533223f02521ae0f0
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-08 14:37:32 +0700

    Update ch0-6-plottranslations.jl

--- LasthrimProjection/Julia-Calculus/ch0-6-plottranslations.jl
+++ LasthrimProjection/Julia-Calculus/ch0-6-plottranslations.jl
@@ -6,9 +6,9 @@ g(x) = abs(x-3)
 h(x) = abs(x)+2
 i(x) = abs(x-3)+2
 
-p1 = plot(f, -108, 108)
-p2 = plot(g, -108, 108)
-p3 = plot(h, -108, 108)
-p4 = plot(i, -108, 108)
+p1 = plot(f, -8, 8)
+p2 = plot(g, -8, 8)
+p3 = plot(h, -8, 8)
+p4 = plot(i, -8, 8)
 
 plot(p1, p2, p3, p4, layout = (2, 2), label=["y = |x|" "y = |x-3|" "y = |x|+2" "y = |x-3|+2"])
