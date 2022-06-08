721bfeb4058105f63c641d18b2a23906c6f9fa00
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-08 17:55:54 +0700

    Update ch0-6-plottranslations2.jl

--- LasthrimProjection/Julia-Calculus/ch0-6-plottranslations2.jl
+++ LasthrimProjection/Julia-Calculus/ch0-6-plottranslations2.jl
@@ -11,8 +11,11 @@ p2 = plot(g, -8, 8)
 p3 = plot(h, -8, 8)
 p4 = plot(i, -8, 8)
 
-s = L"\sqrt{x}";
+s1 = L"y = \sqrt{x}";
+s2 = L"y = \sqrt{x-3}";
+s3 = L"y = \sqrt{x}+2";
+s4 = L"y = \sqrt{x-3}+2";
 
-plot(p1, p2, p3, p4, layout = (2, 2), label=["y = s" "y = L"\sqrt{x-3}"" "y = L"\sqrt{x}"+2" "y = L"\sqrt{x-3}"+2"], 
+plot(p1, p2, p3, p4, layout = (2, 2), xaxis = "x", yaxis = s1, label=[s1 s2 s3 s4], 
 title=["Original function" "Translated 3 units to the right" "Translated upward by 2 units" "Translated 3 units to the right then 2 units upward"],
 titleloc = :bottom, titlefont = font(8))
