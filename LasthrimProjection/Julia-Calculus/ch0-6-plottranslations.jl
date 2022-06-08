8d2ccf0f37e8100b0ef4c82ef70964d509f6b97a
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-08 14:44:52 +0700

    Update ch0-6-plottranslations.jl

--- LasthrimProjection/Julia-Calculus/ch0-6-plottranslations.jl
+++ LasthrimProjection/Julia-Calculus/ch0-6-plottranslations.jl
@@ -11,4 +11,6 @@ p2 = plot(g, -8, 8)
 p3 = plot(h, -8, 8)
 p4 = plot(i, -8, 8)
 
-plot(p1, p2, p3, p4, layout = (2, 2), label=["y = |x|" "y = |x-3|" "y = |x|+2" "y = |x-3|+2"])
+plot(p1, p2, p3, p4, layout = (2, 2), label=["y = |x|" "y = |x-3|" "y = |x|+2" "y = |x-3|+2"], 
+title=["Original function" "Translated 3 units to the right" "Translated upward by 2 units" "Translated 3 units to the right then 2 units upward"],
+titleloc = :bottom, titlefont = font(8))
