f2fba26f75bb5694438ec77ff6266b7325b5b25d
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-11 22:57:35 +0700

    Update ch0-7-plottunitcircle.jl

--- LasthrimProjection/Julia-Calculus/ch0-7-plottunitcircle.jl
+++ LasthrimProjection/Julia-Calculus/ch0-7-plottunitcircle.jl
@@ -8,5 +8,14 @@ end
 
 plot(circleShape(0, 0, 1), seriestype = [:shape,], lw = 0.5,
       c =:blue, linecolor = :black,
-      legend = false, fillalpha = 0.2, aspect_ratio = 1)
-    
+      legend = false, fillalpha = 0.2, aspect_ratio = 1, showaxis = false)
+plot!([0], seriestype="vline", color=:green, label="")
+plot!([0], seriestype="hline", color=:green, label="")
+
+annotate!([(1.1,0, ("x", 10, :red)), 
+           (0.15,1.03, ("y", 10, :red)), 
+           (1.15,-0.1, ("A(1,0)", 10, :red)),
+           (0.1,-0.1, ("(0,0)", 10, :red)),
+           (0.51,0.95, ("(x,y)", 10, :red))])
+
+scatter!([0.50], [0.866025404], color = "red", label="", markersize = 5)
