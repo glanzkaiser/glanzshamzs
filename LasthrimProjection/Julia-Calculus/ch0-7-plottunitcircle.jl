6836176b18f6f31af2fadb029071d72000a13909
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-11 18:08:31 +0700

    Create ch0-7-plottunitcircle.jl

---
+++ LasthrimProjection/Julia-Calculus/ch0-7-plottunitcircle.jl
@@ -0,0 +1,12 @@
+using Plots
+pyplot()
+
+function circleShape(h, k, r)
+     θ = LinRange(0, 2*π, 500)
+     h .+ r*sin.(θ), k .+ r*cos.(θ)
+end
+
+plot(circleShape(0, 0, 1), seriestype = [:shape,], lw = 0.5,
+      c =:blue, linecolor = :black,
+      legend = false, fillalpha = 0.2, aspect_ratio = 1)
+    
