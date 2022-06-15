5b0cc8c61c3220f08623f1a9d693300465c539e8
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-15 15:22:45 +0700

    Create ch0-7-plottunitcircle4.jl

---
+++ LasthrimProjection/Julia-Calculus/ch0-7-plottunitcircle4.jl
@@ -0,0 +1,31 @@
+using Plots, LaTeXStrings
+pyplot()
+
+function circleShape(h, k, r)
+     θ = LinRange(0, 2*π, 500)
+     h .+ r*sin.(θ), k .+ r*cos.(θ)
+end
+
+plot(circleShape(0, 0, 1), seriestype = [:shape,], lw = 0.5,
+      c =:blue, linecolor = :black,
+      legend = false, fillalpha = 0.2, aspect_ratio = 1, showaxis = false)
+
+f(x) = x
+
+plot!([0], seriestype="vline", color=:green, label="")
+plot!([0], seriestype="hline", color=:green, label="")
+plot!([0.707106781,0.707106781],[0.707106781,0], linecolor=:green, linestyle=:dash)
+plot!(f, linecolor=:green, linestyle=:dash)
+
+annotate!([(1.1,0, ("x", 10, :red)), 
+           (0.15,1.03, ("y", 10, :red)), 
+           (0.1,-0.1, ("(0,0)", 10, :red)),
+           (-0.88,-1, ("y=x", 10, :red)),
+           (0.42,0.073, ("x", 10, :red)),
+           (0.66,0.33, ("x", 10, :red)),
+           (0.37,0.46, ("1", 10, :red)),
+           (1.08,-0.08, ("(1,0)", 10, :red)),
+           (0.73,0.87, ("P", 10, :red))])
+
+scatter!([1], [0], color = "red", label="", markersize = 5)
+scatter!([0.707106781], [0.707106781], color = "red", label="", markersize = 5)
