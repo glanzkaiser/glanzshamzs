138e560f1f61dde35826f5e025a5785b6873f0f0
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-15 17:57:37 +0700

    Create ch0-7-plotsincoswithpi.jl

---
+++ LasthrimProjection/Julia-Calculus/ch0-7-plotsincoswithpi.jl
@@ -0,0 +1,32 @@
+using Plots, LaTeXStrings
+pyplot(fmt=:svg)
+
+function pitick(start, stop, denom; mode=:text)
+    a = Int(cld(start, π/denom))
+    b = Int(fld(stop, π/denom))
+    tick = range(a*π/denom, b*π/denom; step=π/denom)
+    ticklabel = piticklabel.((a:b) .// denom, Val(mode))
+    tick, ticklabel
+end
+
+function piticklabel(x::Rational, ::Val{:text})
+    iszero(x) && return "0"
+    S = x < 0 ? "-" : ""
+    n, d = abs(numerator(x)), denominator(x)
+    N = n == 1 ? "" : repr(n)
+    d == 1 && return S * N * "π"
+    S * N * "π/" * repr(d)
+end
+
+function piticklabel(x::Rational, ::Val{:latex})
+    iszero(x) && return L"0"
+    S = x < 0 ? "-" : ""
+    n, d = abs(numerator(x)), denominator(x)
+    N = n == 1 ? "" : repr(n)
+    d == 1 && return L"%$S%$N\pi"
+    L"%$S\frac{%$N\pi}{%$d}"
+end
+
+a, b = -2π, 2π
+plot(sin, a, b; xtick=pitick(a, b, 4), label="y = sin t", size=(720, 250))
+plot(cos, a, b; xtick=pitick(a, b, 4), label="y = cos t", size=(720, 250))
