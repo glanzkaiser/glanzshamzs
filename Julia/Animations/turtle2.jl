104e3c1844f783c32b8038ec33e519381cc28925
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-18 19:58:08 +0700

    Update turtle2.jl

--- Julia/Animations/turtle2.jl
+++ Julia/Animations/turtle2.jl
@@ -49,3 +49,25 @@ end
     Message(t, "$i")
   end
 end
+
+function square(t)
+  for i in 1:4
+    Forward(t, 200)
+    Turn(t, 90)
+  end
+end
+
+
+@draw begin
+  background("antiquewhite")
+  t = Turtle()
+  n = 10
+  Penwidth(t, 0.5)
+  Pencolor(t, "darkblue")
+  for i in 1:n
+    square(t)
+    Turn(t, 360/n)
+  end
+  Message(t, "Freya")
+end
+finish()
