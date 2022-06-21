5f12d5ae44633f6527fd1db152140ff50b8f7a19
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-21 20:06:31 +0700

    Create ch0-7-plotproblemsetno40.jl

---
+++ LasthrimProjection/Julia-Calculus/ch0-7-plotproblemsetno40.jl
@@ -0,0 +1,23 @@
+using Plots, LaTeXStrings
+pyplot()
+
+t = L"$/theta$""
+t1 = L"$/theta_1$""
+t2 = L"$/theta_2$""
+f(x) = x-1
+g(x) = 3.33*x-5
+l1 = L"$l_1$"
+l2 = L"$l_2$"
+
+plot!([0], seriestype="vline", color=:black, legend=false)
+plot!([0], seriestype="hline", color=:black)
+plot!(f, linecolor=:green)
+plot!(g, linecolor=:green)
+plot!(Plots.partialcircle(0,1//14*pi,50,1.33), color=:red, arrow=true)
+plot!(Plots.partialcircle(0,1//11*pi,100,1.73), color=:red, arrow=true)
+     
+annotate!([(1.81,0.9, (t, 8, :red)), 
+           (2.05,1.07, (l1, 8, :red)),
+           (2.05,1.66, (l2, 8, :red)),
+           (1.37,0.173, (t1, 8, :red)),
+           (1.75,0.23, (t2, 8, :red))])
