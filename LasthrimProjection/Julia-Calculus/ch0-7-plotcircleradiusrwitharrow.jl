f6facfafcceb589c91bf3a8d18fac4006d9c4373
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-22 17:20:25 +0700

    Update ch0-7-plotcircleradiusrwitharrow.jl

--- LasthrimProjection/Julia-Calculus/ch0-7-plotcircleradiusrwitharrow.jl
+++ LasthrimProjection/Julia-Calculus/ch0-7-plotcircleradiusrwitharrow.jl
@@ -14,12 +14,11 @@ t = L"$\theta$"
 
 plot!([0], seriestype="vline", color=:green, label="")
 plot!([0], seriestype="hline", color=:green, label="")
-plot!([-0.707106781,0],[-0.707106781,0], linecolor=:green)
-plot!(Plots.partialcircle(0,1//4*pi,10,0.1), color=:red, arrow=true)
-plot!(Plots.partialcircle(0,1//4*pi), color=:red, linewidth=2)
+plot!([-0.707106781,0],[0.707106781,0], linecolor=:green)
+plot!(Plots.partialcircle(0,3//4*pi,50,0.3), color=:red, arrow=true)
      
-annotate!([(0.17,005, (t, 10, :red)),
-           (-0.88,0.81, ("(x,y)", 10, :red)),
-           (-0.51,0.37, ("r", 10, :red))])
+annotate!([(0.17,005, (t, 14, :red)),
+           (-0.85,0.77, ("(x,y)", 14, :red)),
+           (-0.51,0.37, ("r", 14, :red))])
 
-scatter!([0.707106781], [0.707106781], color = "red", label="", markersize = 5)
+scatter!([-0.707106781], [0.707106781], color = "red", label="", markersize = 5)
