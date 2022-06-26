e9da212c642014ac2b675167edcf7feb157be71a
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-26 17:43:56 +0700

    Update ch0-7-plotproblemsetno50.jl

--- LasthrimProjection/Julia-Calculus/ch0-7-plotproblemsetno50.jl
+++ LasthrimProjection/Julia-Calculus/ch0-7-plotproblemsetno50.jl
@@ -6,12 +6,12 @@ pyplot()
 f(x) = 2*sin.(pi*x/5)
 g(x) = sin.(pi*x/5) + cos.(pi*x/5)
 
-p1 = plot(y, -5, 5)
-p2 = plot(y, -5, 5)
+p1 = plot(f, -5, 5)
+p2 = plot(g, -5, 5)
 
 s1 = L"y(t) = 2 sin(\pi t/5)";
 s2 = L"y(t) = sin(\pi t/5) + cos(\pi t/5)";
 
-plot(p1, p2, layout = (1, 1), xaxis = "x", yaxis = s1, label=[s1 s2], 
+plot(p1, p2, layout = (2, 1), xaxis = "x", yaxis = s1, label=[s1 s2], 
 title=["Sum of two sine" "Sum of sine and cosine"],
 titleloc = :bottom, titlefont = font(8))
