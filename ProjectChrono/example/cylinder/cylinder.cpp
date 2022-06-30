9b2e8ddf3a65f54e41d8b55e79c70cd06bb0d044
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-06-30 14:07:46 +0700

    Create cylinder.cpp

---
+++ ProjectChrono/example/cylinder/cylinder.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <chrono>
+#include "chrono/utils/ChUtilsSamplers.h"
+
+using namespace chrono;
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    float r_particle = 1.;
+    float r_cylinder = 20.;
+    float z_cylinder = 10.;
+
+    chrono::utils::PDSampler<float> sampler(2 * r_particle);
+    std::vector<ChVector<float>> positions;
+    auto points = sampler.SampleCylinderZ((0.,0.,z_cylinder), r_cylinder, 0.0);
+    positions.insert(positions.end(), points.begin(), points.end());
+
+    for (int k = 0; k < positions.size(); k++)
+    {
+        std::cout << "R: " << sqrt(pow(positions[k][0], 2) + pow(positions[k][1], 2)) << "\t\tZ:" << positions[k][2] << std::endl;
+    }
+}
