db812c27efa58d78c57f096220ed05202e748503
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-02 21:53:57 +0700

    Update plotunitcircleimaginary.jl

--- LasthrimProjection/Julia-Calculus/plotunitcircleimaginary.jl
+++ LasthrimProjection/Julia-Calculus/plotunitcircleimaginary.jl
@@ -2,6 +2,7 @@ using Plots
 pyplot()
 
 z = exp.(range(0, 2π, 65)im)
-plot(z; title="Circle", legend=:bottomright, labels="z")
+plot(z; title="Circle", legend=:bottomright, labels="z", 
+      marker=:o, aspectratio=:equal, size=(400, 400))
 scatter!(z; xlabel="Re", ylabel="Im", labels=nothing)
 savefig("plot1.png")
