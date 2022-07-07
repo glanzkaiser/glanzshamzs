6f0630c93b480a97a9450e62c886f161cb39d2e9
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-07 20:18:11 +0700

    Update ch4-1-calculatearea.jl

--- LasthrimProjection/Julia-Calculus/ch4-1-calculatearea.jl
+++ LasthrimProjection/Julia-Calculus/ch4-1-calculatearea.jl
@@ -1,3 +1,6 @@
+using Plots, LaTeXStrings
+pyplot()
+
 function Area(f, a, b, n)
 xs = a:(b-a)/n:b
 deltas = diff(xs)
@@ -5,4 +8,6 @@ cs = xs[1:end-1]
 sum(f(cs[i]) * deltas[i] for i in 1:length(deltas))
 end
 
+f(x) = x^2
 Area(x -> x^2, 0, 1, 50_000)
+plot(f,0,1, label=L"y = x^2", legend=:outertop)
