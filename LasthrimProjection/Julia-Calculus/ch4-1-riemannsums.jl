17087f0f2e240f6f57455c0b72ac4370506a7da3
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-08 20:17:40 +0700

    Update ch4-1-riemannsums.jl

--- LasthrimProjection/Julia-Calculus/ch4-1-riemannsums.jl
+++ LasthrimProjection/Julia-Calculus/ch4-1-riemannsums.jl
@@ -1,5 +1,5 @@
 using Plots, LaTeXStrings, ValidatedNumerics
-gr()
+pyplot()
 
 f(x) = √(4 - x^2)
 
@@ -14,7 +14,7 @@ end
 intervals = make_intervals()
 
 # Plot Riemann Sums
-intervals = make_intervals(30)
+intervals = make_intervals(33)
 
 t = L"\sqrt{4 - x^2}"
 p = plot(aspect_ratio=:equal)
@@ -26,3 +26,27 @@ for X in intervals
 end
 
 plot!(f, 0, 2, label=t)
+
+
+#=
+# Sum up the areas of each partition of the rectangle to approximate the area of one quadrant of a circle 
+N = 20
+intervals = make_intervals(N)
+
+width = 2/N
+width * sum(√(4 - X^2) for X in intervals)
+
+# The area approximation for each partition from 50 to 1000
+# Increasing the number of sub-intervals makes better approximation
+
+setdisplay(:standard, sigfigs=5)
+
+println("N \t area interval \t \t diameter")
+for N in 50:50:1000
+    intervals = make_intervals(N)
+    area = (2/N) * sum(√(4 - X^2) for X in intervals)
+
+    println("$N \t $area \t $(diam(area))")
+end
+
+=#
