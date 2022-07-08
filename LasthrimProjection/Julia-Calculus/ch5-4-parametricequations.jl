27de6ee1e583d82bc867d97ffb37cf3f37b7508f
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-08 20:46:43 +0700

    Update ch5-4-parametricequations.jl

--- LasthrimProjection/Julia-Calculus/ch5-4-parametricequations.jl
+++ LasthrimProjection/Julia-Calculus/ch5-4-parametricequations.jl
@@ -3,11 +3,14 @@ pyplot()
 
 g(x, a) = a * x^3 * (1 - x);
 
+f(x) = x^3 * (1 - x)
+
 fig = plot(ratio=1, xlims=(0, 1), ylims=(0, 1)); # produces an empty plot
     
-for a in (1, 2, 3.2, 3.8) 
+for a in (0.51, 2, 3.2, 3.8) 
     plot!(fig, 0:0.01:1, x -> g(x, a), label=L"a = %$a", legend=:outerright) # the loop fills in the plot with this
 end
 
-plot!(x -> x, ls=:dash, label=L"y = a x^{3} (1 - x)", legend=:outerright)
+plot!(f, ls=:dash, label=L"y = x^{3} (1 - x)", legend=:outerright)
 
+# plot!(x -> x, ls=:dash, label=L"y = x^{3} (1 - x)", legend=:outerright)
