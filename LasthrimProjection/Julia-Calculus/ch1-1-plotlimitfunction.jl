eb9ea7ad13ce18eeb9dafbfc95702a62ce33fedf
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-11 14:28:48 +0700

    Update ch1-1-plotlimitfunction.jl

--- LasthrimProjection/Julia-Calculus/ch1-1-plotlimitfunction.jl
+++ LasthrimProjection/Julia-Calculus/ch1-1-plotlimitfunction.jl
@@ -1,8 +1,8 @@
 using Plots, LaTeXStrings
 pyplot()
 
-#f(x) = sin.(x)/(x)
-f(x) = (x^3 - 1) / (x-1)
+f(x) = sin.(x)/(x)
+#f(x) = (x^3 - 1) / (x-1)
 
 plot(f,-30,30, label=L"\frac{x^3 - 1}{x-1}", legend=:outertop)
 plot!([0], seriestype="vline", color=:green, label="")
@@ -10,3 +10,10 @@ plot!([0], seriestype="hline", color=:green, label="")
 
 annotate!([(30.3,23.3, ("x", 10, :black)), 
            (3.03,933.07, ("y", 10, :black))])
+
+println("x \t sin(x)/x ")
+
+for N in -1:0.1:10
+    g = sin.(N) / N
+    println("$N \t $g ")
+end
