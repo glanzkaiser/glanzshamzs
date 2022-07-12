eab1a8dee1381a194cf291dbc654d4a7511f834e
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-12 19:52:00 +0700

    Update ch1-1-plotstepfunction.jl

--- LasthrimProjection/Julia-Calculus/ch1-1-plotstepfunction.jl
+++ LasthrimProjection/Julia-Calculus/ch1-1-plotstepfunction.jl
@@ -8,3 +8,14 @@ for x in xp
     scatter!(x, floor.(x), ms=3, msc=:black, mc=[:black, :white], label=false)
 end
 p
+
+#Alternative plot
+#=
+x = -12:12
+xp = [[i, i+1 - 5*eps(), NaN] for i in x[1:end-1]]
+xx = collect(Iterators.flatten(xp))
+yy = floor.(xx)
+
+plot(xx, yy, c=:black, label=false, framestyle = :origin)
+scatter!(xx', yy', msc=:black, mc=[:black :white :white], ms=3)
+=#
