967b9489d816670f819d2d52757f249bbb26a151
Glanz <72222484+glanzkaiser@users.noreply.github.com>
2022-07-25 12:17:08 +0700

    Update surfaceplot-plotlyjs.jl

--- LasthrimProjection/Julia-Calculus/surfaceplot-plotlyjs.jl
+++ LasthrimProjection/Julia-Calculus/surfaceplot-plotlyjs.jl
@@ -1,9 +1,19 @@
-using Plots, LaTeXStrings, Plots.PlotMeasures 
+using LaTeXStrings, Plots
 plotlyjs()
 
 x = collect(-5:0.5:5)
 y = collect(-5:0.5:5)
 
-z(x, y) = 2*x - 3*y + 2
+z(x, y) = x^2 - 3*y + 2
 
-plot(x, y, z, st = :surface, camera = (10, 10))
+l = L"x^2 - 3y + 2"
+
+plot(x, y, z, st = :surface, camera = (10, 10),
+           extra_plot_kwargs = KW(
+               :include_mathjax => "cdn",
+               :yaxis => KW(:automargin => true),
+               :xaxis => KW(:domain => "auto")
+               ),
+       )
+title!(l)
+savefig("test.html")
